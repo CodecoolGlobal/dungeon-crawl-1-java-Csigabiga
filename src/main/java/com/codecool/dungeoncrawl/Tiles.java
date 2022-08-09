091957df6bf30ecb8e20c87e58112b833ada5419
Qephetzial <prekopadominik@gmail.com>
2022-08-09 15:35:17 +0200

    rebase

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -11,7 +11,7 @@ public class Tiles {
     public static int TILE_WIDTH = 32;
 
     private static Image tileset = new Image("/tiles.png", 543 * 2, 543 * 2, true, false);
-    private static Map<String, Tile> tileMap = new HashMap<>();
+    public static Map<String, Tile> tileMap = new HashMap<>();
     public static class Tile {
         public final int x, y, w, h;
         Tile(int i, int j) {

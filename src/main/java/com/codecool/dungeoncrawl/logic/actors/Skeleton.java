1785ee558a2570fce19d51ec0dff9d356fbb0b9b
Csaba Kádár <sablablab@gmail.com>
2022-08-09 13:52:22 +0200

    ADD: more stat to Skeleton constructor

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -5,6 +5,9 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
+        this.setHealth(5);
+        setAttackPower(3);
+        setDefensePower(0);
     }
 
     @Override

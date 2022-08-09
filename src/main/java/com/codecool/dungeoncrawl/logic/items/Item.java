13a0b9b3dc0f070a5cbdd88a51d2dd568ba9584a
Tóth Gabriella <thgabi1@gmail.com>
2022-08-09 14:59:25 +0200

    ADD: addToInventory method

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -3,8 +3,11 @@ package com.codecool.dungeoncrawl.logic.items;
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
+import java.util.ArrayList;
+
 public abstract class Item implements Drawable {
     private Cell cell;
+    private ArrayList<Item> items = new ArrayList<>();
 
     public Item(Cell cell) {
         this.cell = cell;
@@ -13,5 +16,9 @@ public abstract class Item implements Drawable {
 
     public Cell getCell() {
         return cell;
+    }
+
+    public void addToInventory(Item item) {
+        items.add(item);
     }
 }

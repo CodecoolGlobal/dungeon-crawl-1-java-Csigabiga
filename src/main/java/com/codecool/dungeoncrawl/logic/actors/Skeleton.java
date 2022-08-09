23d9d818217f6a607aa94adccb1bfdd2fcf4da60
Csaba Kádár <sablablab@gmail.com>
2022-08-09 16:28:06 +0200

    EDIT: skeleton stats

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -5,8 +5,8 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
-        this.setHealth(5);
-        setAttackPower(3);
+        this.setHealth(10);
+        setAttackPower(2);
         setDefensePower(0);
     }
 

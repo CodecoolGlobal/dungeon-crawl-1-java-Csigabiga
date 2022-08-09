8b120aed792c4c9baa7a1aa395351f6b10757c04
Csaba Kádár <sablablab@gmail.com>
2022-08-09 13:43:13 +0200

    EDIT: playerMove can now move to other style of tiles

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -17,7 +17,8 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (nextCell.getType().equals(CellType.FLOOR) && nextCell.getActor() == null) {
+        if (nextCell.getType().getTileName().matches("floor|floor1|floor2") &&
+             nextCell.getActor() == null) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;

9fdfb8cad388f8524c37aed934110a075700f775
Csaba Kádár <sablablab@gmail.com>
2022-08-09 13:51:13 +0200

    ADD: more stat to player constructor

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -5,6 +5,9 @@ import com.codecool.dungeoncrawl.logic.Cell;
 public class Player extends Actor {
     public Player(Cell cell) {
         super(cell);
+        this.setHealth(10);
+        this.setAttackPower(3);
+        this.setDefensePower(1);
     }
 
     public String getTileName() {

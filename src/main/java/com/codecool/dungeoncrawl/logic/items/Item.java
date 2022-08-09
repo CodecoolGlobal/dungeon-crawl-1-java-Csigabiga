1304797c9ab2bb172d865c1882f07ba7fb45ec80
Tóth Gabriella <thgabi1@gmail.com>
2022-08-09 16:39:45 +0200

    ADD: display inventory

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -4,10 +4,11 @@ import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
 import java.util.ArrayList;
+import java.util.StringJoiner;
 
 public abstract class Item implements Drawable {
     private Cell cell;
-    private ArrayList<Item> items = new ArrayList<>();
+    private static final ArrayList<Item> items = new ArrayList<>();
 
     public Item(Cell cell) {
         this.cell = cell;
@@ -18,7 +19,15 @@ public abstract class Item implements Drawable {
         return cell;
     }
 
-    public void addToInventory(Item item) {
+    public static void addToInventory(Item item) {
         items.add(item);
+    }
+
+    public static String display() {
+        StringJoiner stringJoiner = new StringJoiner("\n");
+        for (Item item: items) {
+            stringJoiner.add(item.getClass().getSimpleName());
+        }
+        return stringJoiner.toString();
     }
 }

481cd81e24d7bd1077d88d0435683fa711a6cb4c
Tóth Gabriella <thgabi1@gmail.com>
2022-08-24 14:34:26 +0200

    ADD: basic db operations

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -5,27 +5,86 @@ import com.codecool.dungeoncrawl.model.PlayerModel;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
+    private DataSource dataSource;
+
+    public GameStateDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
     @Override
     public void add(GameState state) {
+        Date currentDate = new Date(System.currentTimeMillis());
 
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO game_state (current_map, saved_at, player_id) VALUES (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, state.getCurrentMap());
+            statement.setDate(2, currentDate);
+            statement.setInt(3, state.getId());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            resultSet.next();
+            state.setId(resultSet.getInt(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(GameState state) {
+        Date currentDate = new Date(System.currentTimeMillis());
 
+        try(Connection connection = dataSource.getConnection()) {
+            String sql = "UPDATE game_state SET current_map = ?, saved_at = ?, player_id = ? WHERE id = ?";
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, state.getCurrentMap());
+            st.setDate(2, currentDate);
+            st.setInt(3, state.getId());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public GameState get(int id) {
+        try(Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT current_map, saved_at, player_id FROM game_state WHERE id = ?";
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (!rs.next()) {
+                return null;
+            }
+/*            GameState gameState = new GameState(rs.getString(1), rs.getString(2), rs.getDate(3));
+            gameState.setId(id);
+            return gameState;*/
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading gamestates with id: " + id, e);
+        }
         return null;
     }
 
     @Override
     public List<GameState> getAll() {
-        return null;
+        try(Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT current_map, saved_at, player_id FROM game_state";
+            ResultSet rs = connection.createStatement().executeQuery(sql);
+            List<GameState> result = new ArrayList<>();
+            //while (rs.next()) {
+                /*GameState gameState = new GameState(rs.getString(2), rs.getDate(3), rs.getObject(4));
+                gameState.setId(rs.getInt(1));
+                result.add(gameState);*/
+            //}
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all gamestates", e);
+        }
     }
 }
--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -17,12 +17,18 @@ public class PlayerDaoJdbc implements PlayerDao {
     public void add(PlayerModel player) {
         try (Connection conn = dataSource.getConnection()) {
             String sql = "INSERT INTO player (player_name, hp, x, y) VALUES (?, ?, ?, ?)";
+            // A SQL statement is precompiled and stored in a PreparedStatement object.
+            // This object can then be used to efficiently execute this statement multiple times.
             PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, player.getPlayerName());
             statement.setInt(2, player.getHp());
             statement.setInt(3, player.getX());
             statement.setInt(4, player.getY());
             statement.executeUpdate();
+            // A ResultSet object maintains a cursor pointing to its current row of data.
+            // Initially the cursor is positioned before the first row.
+            // The next method moves the cursor to the next row, and because it returns false when there are no
+            // more rows in the ResultSet object, it can be used in a while loop to iterate through the result set.
             ResultSet resultSet = statement.getGeneratedKeys();
             resultSet.next();
             player.setId(resultSet.getInt(1));

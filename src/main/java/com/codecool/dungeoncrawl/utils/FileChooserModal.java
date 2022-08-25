0ab991102162b9099f82ec32f4592943b26cc44d
Csaba Kádár <sablablab@gmail.com>
2022-08-25 15:03:16 +0200

    ADD: FileChooserModal

--- src/main/java/com/codecool/dungeoncrawl/utils/FileChooserModal.java
+++ src/main/java/com/codecool/dungeoncrawl/utils/FileChooserModal.java
@@ -1,9 +1,5 @@
 package com.codecool.dungeoncrawl.utils;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.File;

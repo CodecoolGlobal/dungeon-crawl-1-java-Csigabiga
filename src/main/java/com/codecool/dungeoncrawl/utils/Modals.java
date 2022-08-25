e8bf81e8aa66276abcdddc3d641f34b059ebbbce
Tóth Gabriella <thgabi1@gmail.com>
2022-08-25 14:52:13 +0200

    Changes by csaba

--- src/main/java/com/codecool/dungeoncrawl/utils/Modals.java
+++ src/main/java/com/codecool/dungeoncrawl/utils/Modals.java
@@ -8,39 +8,39 @@ import java.util.List;
 import java.util.Optional;
 
 public class Modals {
-//    public static String showModal(Button button, Player player, GameDatabaseManager dbmanager) {
-//        button.setOnAction(actionEvent -> {
-//            String result="";
-//            final Stage dialog = new Stage();
-//            dialog.setTitle("Save");
-//            dialog.initModality(Modality.APPLICATION_MODAL);
-//            VBox dialogVbox = new VBox(20);
-//            dialogVbox.getChildren().add(new Label("Name:"));
-//            TextField textField = new TextField();
-//            dialogVbox.getChildren().add(textField);
-//            Button saveButton =  new Button("Save");
-//            dialogVbox.getChildren().add(saveButton);
-//            Button cancelButton =  new Button("Cancel");
-//            dialogVbox.getChildren().add(cancelButton);
-//            dialogVbox.setPadding(new Insets(10));
-//            Scene dialogScene = new Scene(dialogVbox, 300, 200);
-//            dialog.setScene(dialogScene);
-//            dialog.showAndWait();
-//            cancelButton.setOnAction(actionEvent1 -> dialog.close());
-//
-//            saveButton.setOnAction(actionEvent1 -> {
-//                //result = "valami";
-////                if (dbmanager.checkPlayerNameInDb(textField.getText())) {
-////                    ConfirmModal.showModal(saveButton, dbmanager, dialog);
-////                } else {
-////                    player.setPlayerName(textField.getText());
-////                    dbmanager.savePlayer(player);
-////                    dialog.close();
-////                }
-//            });
-//        });
-//        return null;
-//    }
+    /*public static String showModal(Button button, Player player, GameDatabaseManager dbmanager) {
+        button.setOnAction(actionEvent -> {
+            String result="";
+            final Stage dialog = new Stage();
+            dialog.setTitle("Save");
+            dialog.initModality(Modality.APPLICATION_MODAL);
+            VBox dialogVbox = new VBox(20);
+            dialogVbox.getChildren().add(new Label("Name:"));
+            TextField textField = new TextField();
+            dialogVbox.getChildren().add(textField);
+            Button saveButton =  new Button("Save");
+            dialogVbox.getChildren().add(saveButton);
+            Button cancelButton =  new Button("Cancel");
+            dialogVbox.getChildren().add(cancelButton);
+            dialogVbox.setPadding(new Insets(10));
+            Scene dialogScene = new Scene(dialogVbox, 300, 200);
+            dialog.setScene(dialogScene);
+            dialog.showAndWait();
+            cancelButton.setOnAction(actionEvent1 -> dialog.close());
+
+            saveButton.setOnAction(actionEvent1 -> {
+                result = "valami";
+                if (dbmanager.checkPlayerNameInDb(textField.getText())) {
+                    ConfirmModal.showModal(saveButton, dbmanager, dialog);
+                } else {
+                    player.setPlayerName(textField.getText());
+                    dbmanager.savePlayer(player);
+                    dialog.close();
+                }
+            });
+        });
+        return null;
+    }*/
 
     public static String inputDialog() {
         TextInputDialog dialog = new TextInputDialog("player");
